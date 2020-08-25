03daead251ef8bf39c91d6adc9111144fbc5e989
RezwanAhmed <rezwan2387@gmail.com>
2020-08-25 16:35:27 +0600

    Safety Assistance Launce Page

---
+++ app/src/main/java/com/example/practicetwo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.practicetwo;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
